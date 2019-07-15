ea5a932aa48eecbb36d6be203b6590c0ca6754e4
GiorgioDeveloper <51720485+GiorgioDeveloper@users.noreply.github.com>
2019-07-15 22:30:25 +0200

    Add files via upload

---
+++ Javascript_Fun.Array.js
@@ -0,0 +1,216 @@
+// Finding longest word
+
+var word2 = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orchard',
+  'crackpot'
+];
+
+function findLongestWord (words) {
+  var longest = "";
+
+   for(let i = 0; i < words.length; i++){
+    
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+ }
+ return longest;
+};
+
+console.log(findLongestWord(word2))
+
+
+//Calculating a Sum
+
+var numbers2 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumArray (numbers) {
+  let sum = 0;
+
+   for(let i = 0; i < numbers.length; i++) {
+
+    sum += numbers[i];
+   }
+   return sum;
+}
+console.log(sumArray(numbers2));
+
+
+// Calculate average Level 1 array of numbers
+
+var numbers3 = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers (numbers) {
+  let avg = sumArray(numbers) / numbers.length;
+   return avg;
+}
+console.log(averageNumbers(numbers3));
+
+
+// Calculate average Level 2 array of strings
+
+var words3 = [
+    'seat',
+    'correspond',
+    'linen',
+    'motif',
+    'hole',
+    'smell',
+    'smart',
+    'chaos',
+    'fuel',
+    'palace'
+];
+
+function averageWordLength (wordz) {
+  let avgWord = 0;
+   for(let i = 0; i < wordz.length; i++) {
+    avgWord += wordz[i].length;
+   }
+   return avgWord / wordz.length ;
+}
+console.log(averageWordLength(words3));
+
+
+// Unique Arrays
+
+var words4 = [
+  'crab',
+  'poison',
+  'contagious',
+  'simple',
+  'bring',
+  'sharp',
+  'playground',
+  'poison',
+  'communion',
+  'simple',
+  'bring'
+];
+
+function uniquifyArray (wordss){
+  var unique = [];
+
+   for(let i = 0; i < wordss.length; i++) {
+      
+     if (unique.indexOf(wordss[i]) === -1) {
+       unique.push(wordss[i]);
+     }
+   }
+  return unique;
+}
+console.log(uniquifyArray(words4));
+
+
+// Finding Elements
+
+var words5 = [
+  'machine',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'truth',
+  'disobedience'
+];
+
+const search = "eating";
+
+function doesWordExist (wordA) {
+ let output = []
+  for(let i = 0; i < wordA.length; i++){
+    if(search === wordA[i]){
+    output.push(wordA[i]);
+    } 
+  }
+
+  if (output == search) {
+    return true
+  } else { 
+    return false
+  }
+} 
+console.log(doesWordExist(words5));
+
+
+// Counting Repetition
+
+var words6 = [
+  'machine',
+  'matter',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'matter',
+  'truth',
+  'disobedience',
+  'matter'
+];
+
+const search2 = "matter";
+
+function howManyTimes (wordNum){
+ let count = 0
+  for( let i = 0; i < wordNum.length; i++){
+    if(search2 === wordNum[i]){
+    count += 1;
+    } 
+  }
+  return count;
+} 
+console.log(howManyTimes(words6));
+
+
+// Bonus Quest !! NOT FINISHED !!
+
+var matrix1 = [
+  [08,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91,08],
+  [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00],
+  [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65],
+  [52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91],
+  [22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80],
+  [24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50],
+  [32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70],
+  [67,26,20,68,02,62,12,20,95,63,94,39,63,08,40,91,66,49,94,21],
+  [24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72],
+  [21,36,23,09,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95],
+  [78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14,09,53,56,92],
+  [16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57],
+  [86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58],
+  [19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40],
+  [04,52,08,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66],
+  [88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69],
+  [04,42,16,73,38,25,39,11,24,94,72,18,08,46,29,32,40,62,76,36],
+  [20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16],
+  [20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54],
+  [01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48],
+];
+
+function greatestProduct (matrix) {
+
+let product = []
+
+
+  for ( var i = 0; i < matrix.length; i++){
+ var row = matrix[i];
+
+ for (var j = 0; j < row.length; j++){
+    var column = row[j]*row[j+1]*row[j+2]*row[j+3]; {
+      if(column >= 0){
+      product.push(column);
+    }
+ }
+return Math.max(product);
+  }
+ }
+}
+ console.log(greatestProduct(matrix1));
+
